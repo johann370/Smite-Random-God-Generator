ab10c9b1e5f3c85b5f530b81c9aa679eef2598cb
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-04 15:45:15 -0500

    Added function to get a random god from csv

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import subprocess
+import pandas as pd
+from random import randint
+
+def get_gods_list():
+    data = pd.read_csv('gods.csv')
+    return data
+
+def get_random_god(god_list):
+    max = len(god_list) - 1
+    random_number = randint(0, max)
+    random_god = god_list[random_number]
+    return random_god
 
 if __name__ == '__main__':
-    subprocess.call("god-scraper.py", shell=True)
+    #subprocess.call('god-scraper.py', shell=True)
+    god_names = get_gods_list().God
+    random_god = get_random_god(god_names)
+    print(random_god)
