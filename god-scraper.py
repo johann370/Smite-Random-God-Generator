31ed0fbef5ace804f9365470181ce70ba405788c
Johann Mendoza <johannmendoza370@gmail.com>
2021-10-05 21:58:30 -0500

    Updated god-scraper.py to use requests instead of webdriver

--- god-scraper.py
+++ god-scraper.py
@@ -1,40 +1,13 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import pandas as pd
 import urllib.request
 import requests
 
 
 def get_gods():
-    driver = webdriver.Chrome('C:/Users/dragonking370/Desktop/chromedriver_win32/chromedriver.exe')
-    content = driver.get('https://smite.fandom.com/wiki/Smite_Wiki')
-    content = driver.page_source
+    content = requests.get('https://smite.fandom.com/wiki/List_of_gods').content
     soup = BeautifulSoup(content, 'lxml')
-    gods = soup.find_all('div', style='display: inline-block;padding:1px;padding-right:5px')
-    driver.close()
-    godsList = []
-
-    for god in gods:
-        name = god.text
-        a = god.select('span a')
-        for el in a:
-            link = f'https://smite.fandom.com{el["href"]}'
-        godInfo = {
-            'God': name,
-            'Wiki': link
-        }
-        print(godInfo)
-        godsList.append(godInfo)
-    return godsList
-
-
-def get_gods_updated():
-    driver = webdriver.Chrome('C:/Users/dragonking370/Desktop/chromedriver_win32/chromedriver.exe')
-    content = driver.get('https://smite.fandom.com/wiki/List_of_gods')
-    content = driver.page_source
-    soup = BeautifulSoup(content, 'lxml')
-    gods = soup.find('tbody').find_all('tr')
-    driver.close()
+    gods = soup.find('tbody').find_all('tr')[1:]
     gods_list = []
 
     for god in gods:
@@ -64,7 +37,8 @@ def download_god_image(link):
 
     name = god_info[0].text.strip()
 
-    if(name == 'This page refers to content that has yet to be released and may contain inaccuracies. Nothing here is final and anything is subject to change.'):
+    if (name == 'This page refers to content that has yet to be released and may contain inaccuracies. Nothing here '
+                'is final and anything is subject to change.'):
         return
 
     image_link = god_info[1].find('a').get('href')
@@ -78,7 +52,7 @@ def export_gods_to_csv(gods):
 
 
 if __name__ == "__main__":
-    gods = get_gods_updated()
+    gods = get_gods()
     export_gods_to_csv(gods)
     links = list()
 
